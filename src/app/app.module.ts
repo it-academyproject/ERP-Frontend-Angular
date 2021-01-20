840dd9effa8ca7dfb876d00342e172e7016ff826
Oihane Chourraut Yurramendi <oihanechourrautyurramendi@iMac-de-Oihane.local>
2021-01-20 11:09:08 +0100

    header components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './views/header/header.component';
+import { AdminComponent } from './views/header/admin/admin.component';
+import { NotificationsComponent } from './views/header/notifications/notifications.component';
+import { HeaderSearchComponent } from './views/header/header-search/header-search.component';
+import { TogglerComponent } from './views/header/toggler/toggler.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AdminComponent,
+    NotificationsComponent,
+    HeaderSearchComponent,
+    TogglerComponent
   ],
   imports: [
     BrowserModule,
