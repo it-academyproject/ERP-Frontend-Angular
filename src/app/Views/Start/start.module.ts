f3051e370829f09611e2cf0442e1fba76b469af2
isaacBejarano <isaacbejaranopro@gmail.com>
2021-01-21 00:46:00 +0100

    StartModule imported in AppModule

--- src/app/Views/Start/start.module.ts
+++ src/app/Views/Start/start.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { RecoverPasswordComponent } from './recover-password/recover-password.component';
 import { LoginComponent } from './login/login.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+
+import { StartModule } from './Views/Start/start.module';
+
 import { AppComponent } from './app.component';
 import { NavbarClientComponent } from './Components/navbar-client/navbar-client.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarClientComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, NavbarClientComponent],
+  imports: [BrowserModule, AppRoutingModule, StartModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
