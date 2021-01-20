ecde4ba41559e777cafd681fe815551217a780d4
Sanarva <plosky21@hotmail.com>
2021-01-20 18:04:35 +0100

    Initial commit where is taken into account two new components: footer & licence

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { LicenceComponent } from './licence/licence.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    LicenceComponent
   ],
   imports: [
     BrowserModule,
