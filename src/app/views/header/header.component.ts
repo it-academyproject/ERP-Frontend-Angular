20565d080fcdbe2d494f477753cd78baa1e49e80
Oihane Chourraut Yurramendi <oihanechourrautyurramendi@iMac-de-Oihane.local>
2021-01-20 12:15:11 +0100

    Bootstrap 4.6 added

--- src/app/views/header/header.component.ts
+++ src/app/views/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
@@ -6,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
+  faCoffee = faCoffee;
+  
   constructor() { }
 
   ngOnInit(): void {
