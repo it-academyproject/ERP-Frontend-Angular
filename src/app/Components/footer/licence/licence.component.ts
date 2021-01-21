ab6491113525e55431ad4ea782fc4cdeaf6fb1f8
Sanarva <plosky21@hotmail.com>
2021-01-21 11:07:45 +0100

    We add footer component to Component folder

---
+++ src/app/Components/footer/licence/licence.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-licence',
+  templateUrl: './licence.component.html',
+  styleUrls: ['./licence.component.scss']
+})
+export class LicenceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
