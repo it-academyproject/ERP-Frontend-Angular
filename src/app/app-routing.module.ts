dfcf8fb5e47ceb4280a8271a2bbac3005ab30416
Jose Maria Farfan <magoalakai@gmail.com>
2021-01-22 15:51:57 +0100

    App routing modificado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { RecoverPasswordComponent } from '../app/Views/Start/recover-password/recover-password.component';
+import { RecoverPasswordComponent } from './Views/Start/recover-password/recover-password.component';
 import { SignUpComponent } from './Views/Start/sign-up/sign-up.component';
 
 const routes: Routes = [
   { path: 'recover-password', component: RecoverPasswordComponent},
   {path:'sign_up', component:SignUpComponent}
-  
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,13 @@ import { StartModule } from './Views/Start/start.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarClientComponent } from './Components/navbar-client/navbar-client.component';
-import { RecoverPasswordComponent } from './Views/Start/recover-password/recover-password.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarClientComponent,
-      RecoverPasswordComponent
   ],
   imports: [
     BrowserModule,
