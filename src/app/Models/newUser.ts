17d4624f734a6cdec8eb4d94395499833c95f2e9
Jose Maria Farfan <magoalakai@gmail.com>
2021-01-22 02:18:31 +0100

    Mejora validación password

--- src/app/Models/newUser.ts
+++ src/app/Models/newUser.ts
@@ -4,14 +4,12 @@ export class UserSignUp{
   public cif: string;
   public email: string;
   public password: string;
-  public password_conf: string;
 
-  constructor(businessName:string, cif:string, email:string, password:string, password_conf: string){
+  constructor(businessName:string, cif:string, email:string, password:string){
     this.businessName = businessName;
     this.cif = cif;
     this.email = email;
     this.password = password;
-    this.password_conf = password_conf;
   }
 
 }
--- src/app/Views/Start/sign-up/sign-up.component.ts
+++ src/app/Views/Start/sign-up/sign-up.component.ts
@@ -9,9 +9,10 @@ import { UserSignUp } from 'src/app/Models/newUser';
 export class SignUpComponent implements OnInit {
 
   public new_user:UserSignUp;
+  public password_conf:string;
 
   constructor() {
-    this.new_user = new UserSignUp('', '', '','', '');
+    this.new_user = new UserSignUp('', '', '','');
   }
 
   ngOnInit(): void {
@@ -21,13 +22,7 @@ export class SignUpComponent implements OnInit {
   onSubmit(form){
     console.log(this.new_user);
     form.reset();
-    this.enviarAlert();
-  }
-
-  enviarAlert (){
-    const editor: HTMLDivElement = (document.getElementById('alert') as HTMLDivElement);
-    editor.classList.remove('d-none');
-    setTimeout(()=> editor.classList.add('d-none'), 4000);
+    setTimeout(()=> alert('Your registration was succesful!'), 1000);
   }
 
 }
