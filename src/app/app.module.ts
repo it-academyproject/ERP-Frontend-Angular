5e3dd894bb07c2276d7909361099d83804d2016d
oihoihoih <ochyuo@hotmail.com>
2021-01-22 10:12:44 +0100

    header component and its items added to Components folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './views/header/header.component';
-import { AdminComponent } from './views/header/admin/admin.component';
-import { NotificationsComponent } from './views/header/notifications/notifications.component';
-import { HeaderSearchComponent } from './views/header/header-search/header-search.component';
-import { TogglerComponent } from './views/header/toggler/toggler.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { AdminComponent } from './Components/header/admin/admin.component';
+import { NotificationsComponent } from './Components/header/notifications/notifications.component';
+import { HeaderSearchComponent } from './Components/header/header-search/header-search.component';
+import { TogglerComponent } from './Components/header/toggler/toggler.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { NavbarClientComponent } from './Components/navbar-client/navbar-client.component';
 
