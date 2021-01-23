53dbc0bd51ddb3f9907801aa513ccb06b948f231
isaacBejarano <isaacbejaranopro@gmail.com>
2021-01-24 00:23:14 +0100

    login logic TODO list

--- src/app/Views/Start/login/login.component.ts
+++ src/app/Views/Start/login/login.component.ts
@@ -9,11 +9,16 @@ export class LoginComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {
-    var myModal = document.getElementById('staticBackdrop');
-    var myInput = document.getElementById('myInput');
-
-    myModal.addEventListener('shown.bs.modal', function () {
-      myInput.focus();
-    });
+    // to enable focus on modal if you need it //
+    // var myModal = document.getElementById('staticBackdrop');
+    // var myInput = document.getElementById('myInput');
+    // myModal.addEventListener('shown.bs.modal', function () {
+    //   myInput.focus();
+    // });
+    // TODO:
+    // - MockUp login (any data it's okay to login)
+    // -Validations (required + patterns)
+    // -Animations (animation css or B5 or NOTHING)
+    // -Lo que creais conveniente --oR NOTHING
   }
 }
