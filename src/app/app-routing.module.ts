e5b8ff7e111cc870e8b0f9bea695d99a9dc0cdec
isaacBejarano <isaacbejaranopro@gmail.com>
2021-01-23 12:13:44 +0100

    fixed start module components exports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,26 +1,17 @@
-// import { NgModule } from '@angular/core';
-// import { Routes, RouterModule } from '@angular/router';
-// import { RecoverPasswordComponent } from './Views/Start/recover-password/recover-password.component';
-// import { SignUpComponent } from './Views/Start/sign-up/sign-up.component';
-
-// const routes: Routes = [
-//   { path: 'recover-password', component: RecoverPasswordComponent },
-//   { path: 'sign_up', component: SignUpComponent },
-// ];
-
-// @NgModule({
-//   imports: [RouterModule.forRoot(routes)],
-//   exports: [RouterModule],
-// })
-// export class AppRoutingModule {}
-
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './Views/Start/login/login.component';
+import { RecoverPasswordComponent } from './Views/Start/recover-password/recover-password.component';
+import { SignUpComponent } from './Views/Start/sign-up/sign-up.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'recover-password', component: RecoverPasswordComponent },
+  { path: 'sign-up', component: SignUpComponent },
+  { path: 'log-in', component: LoginComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
