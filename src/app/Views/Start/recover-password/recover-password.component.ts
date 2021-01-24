72dd5f1b19c813bdd385efdf4d3c3caf77d9f57a
Clara M <claraaaaamc@gmail.com>
2021-01-24 11:28:51 +0100

    added email verification & disabled button until validation

--- src/app/Views/Start/recover-password/recover-password.component.ts
+++ src/app/Views/Start/recover-password/recover-password.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-recover-password',
@@ -7,9 +9,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecoverPasswordComponent implements OnInit {
 
-  constructor() { }
+	emailCtrl = new FormControl('',[Validators.required, Validators.pattern(/^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/)]);
+
+  constructor() { 
+	  this.emailCtrl.valueChanges
+	  .pipe(
+		  debounceTime(500)
+		  )
+	  .subscribe( value =>{console.log(value);
+	})
+  }
 
   ngOnInit(): void {
   }
 
+  getEmail(event:Event){
+	event.preventDefault();
+	console.log(this.emailCtrl.value);
 }
+
+
+}
+
+
+// function validate(){
+// 	var errorEmail, emailField;
+// 	var emailTest = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+// 	errorEmail = document.getElementById("emailError");
+// 	errorEmail.innerHTML = "";
+// 	emailField = document.getElementById("email").value;
+
+// if(emailTest.test(emailField)==false){
+// 	errorEmail.innerHTML = "Please, enter a valid e-mail";
+// 	}
+// }
