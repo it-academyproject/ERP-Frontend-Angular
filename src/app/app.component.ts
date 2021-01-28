cb6f9564746920271be4e42fd4eae84ff6edf6d6
Loredana Tataru <lory.tataru@gmail.com>
2021-01-28 12:59:50 +0100

    Configured Dev  Environment
    Created the top left message: Dev mode which will appear all the time when working in the development mode.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from '../environments/environment';
 
 //////////////////////////
 // Bootstrap JS imports //
@@ -48,6 +49,11 @@ import {
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
+  // mode is the message that appears in the left top corner: Dev Mode
+  mode = '';
+  constructor() {
+    this.mode = environment.mode;
+  }
   title = 'ITProject-ERP-Frontend';
 
   ngOnInit() {
@@ -55,7 +61,7 @@ export class AppComponent implements OnInit {
 
     // We are only using the user-astronaut icon
     library.add(
-      faUserPlus, 
+      faUserPlus,
       faSignInAlt,
       faCopyright,
       // faBars // TODO: consider it
