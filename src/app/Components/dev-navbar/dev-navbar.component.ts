3758307a5077271bce8e14bf42488f1479ebc74f
isaacBejarano <isaacbejaranopro@gmail.com>
2021-02-08 13:01:12 +0100

    fixed dome spacing in 404, login, and recovery

--- src/app/Components/dev-navbar/dev-navbar.component.ts
+++ src/app/Components/dev-navbar/dev-navbar.component.ts
@@ -3,10 +3,5 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-dev-navbar',
   templateUrl: './dev-navbar.component.html',
-  styleUrls: ['./dev-navbar.component.scss'],
 })
-export class DevNavbarComponent {
-  constructor() {}
-
-  // this component HAS NO LOGIC (it's only a template and potential scss)
-}
+export class DevNavbarComponent {}
