fa0fbf6711f91f1991c7242c4014e73aaec2d0a2
isaacBejarano <isaacbejaranopro@gmail.com>
2021-02-10 20:54:19 +0100

    headers and footer made sticky and fixed

--- src/app/Components/dev-navbar/dev-navbar.component.ts
+++ src/app/Components/dev-navbar/dev-navbar.component.ts
@@ -3,5 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-dev-navbar',
   templateUrl: './dev-navbar.component.html',
+  styleUrls: ['./dev-navbar.component.scss'],
 })
 export class DevNavbarComponent {}
