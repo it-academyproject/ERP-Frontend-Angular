e20d888f80e74cabe583e65fefe2657911824b0d
isaacBejarano <isaacbejaranopro@gmail.com>
2021-02-15 22:04:20 +0100

    provisional admin header auto load

--- src/app/Components/role-sidebar/role-sidebar.component.ts
+++ src/app/Components/role-sidebar/role-sidebar.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-role-sidebar',
   templateUrl: './role-sidebar.component.html',
-  styleUrls: ['./role-sidebar.component.scss']
+  styleUrls: ['./role-sidebar.component.scss'],
 })
 export class RoleSidebarComponent implements OnInit {
+  toggleState = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  toggleSidebar() {
+    this.toggleState = !this.toggleState;
+    console.log(this.toggleState);
   }
-
 }
