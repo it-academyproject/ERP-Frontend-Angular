06ee271d74c1a4fa039451d68d0849d098943afd
Claramc1 <55357651+Claramc1@users.noreply.github.com>
2021-02-17 09:17:21 +0100

    created view

--- src/app/Views/Client/client.module.ts
+++ src/app/Views/Client/client.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from './../../app-routing.module';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 //import components
 import { NewClientComponent } from './new-client/new-client.component';
@@ -15,7 +18,10 @@ import { HttpClientModule } from '@angular/common/http';
     CommonModule,
     AppRoutingModule,
     HttpClientModule,
+    FontAwesomeModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   exports: [NewClientComponent],
 })
-export class ClientModule {}
+export class ClientModule {   }
--- src/app/Views/Client/new-client/new-client.component.ts
+++ src/app/Views/Client/new-client/new-client.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { faPenSquare } from '@fortawesome/free-solid-svg-icons';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new-client',
@@ -6,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-client.component.scss']
 })
 export class NewClientComponent implements OnInit {
+  iconPenSquare = faPenSquare;
+  nameCtrl = new FormControl('',[Validators.required, Validators.pattern(/^[a-z ,.'-]+$/i)]);
+  addressCtrl = new FormControl('',[Validators.required, Validators.pattern(/^[A-Za-z0-9.'\s]*$/)]);
+  cifCtrl = new FormControl('',[Validators.required, Validators.pattern(/^[a-zA-Z]{1}\d{7}[a-zA-Z0-9]{1}$/)]);
 
   constructor() { }
 
