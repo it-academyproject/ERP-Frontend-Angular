f48b941fa91d534550da257d46f075194b8801ae
Claramc1 <55357651+Claramc1@users.noreply.github.com>
2021-02-18 09:29:46 +0100

    added image modal and upload image modal

--- src/app/Views/Client/new-client/new-client.component.ts
+++ src/app/Views/Client/new-client/new-client.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { faPenSquare } from '@fortawesome/free-solid-svg-icons';
+import { faFolder } from '@fortawesome/free-solid-svg-icons';
 import { FormControl, Validators } from '@angular/forms';
 import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 
@@ -11,6 +12,7 @@ import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 export class NewClientComponent implements OnInit {
   closeResult = '';
   iconPenSquare = faPenSquare;
+  iconFolder = faFolder;
   nameCtrl = new FormControl('',[Validators.required, Validators.pattern(/^[a-z ,.'-]+$/i)]);
   addressCtrl = new FormControl('',[Validators.required, Validators.pattern(/^[A-Za-z0-9.',\s]*$/)]);
   cifCtrl = new FormControl('',[Validators.required, Validators.pattern(/^[a-zA-Z]{1}\d{7}[a-zA-Z0-9]{1}$/)]);
@@ -23,6 +25,24 @@ export class NewClientComponent implements OnInit {
   //pop-up informative window
   open(content) {
     this.modalService.open((content), {centered: true, ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  //pop-up upload image
+  openUpload(contentUpload) {
+    this.modalService.open((contentUpload), {centered: true, ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  //Enlarge image
+  openEnlarge(contentEnlarge){
+    this.modalService.open((contentEnlarge), {centered: true, ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
       this.closeResult = `Closed with: ${result}`;
     }, (reason) => {
       this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
