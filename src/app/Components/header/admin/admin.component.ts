2f946a8b94ba0fbd3e9ceda5852ebbeef46a7edc
Fran <rivilla13@gmail.com>
2021-03-04 17:58:47 +0100

    Logout fixed and working

--- src/app/Components/header/admin/admin.component.ts
+++ src/app/Components/header/admin/admin.component.ts
@@ -5,6 +5,7 @@ import {
 } from '@fortawesome/free-regular-svg-icons';
 import { faCog, faPowerOff } from '@fortawesome/free-solid-svg-icons';
 import { LoginService } from 'src/app/Services/login.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin',
@@ -19,10 +20,12 @@ export class AdminComponent implements OnInit {
   faCog = faCog;
   faPowerOff = faPowerOff;
 
-  constructor(private loginService: LoginService) {}
+  constructor(private loginService: LoginService,
+    private router: Router) {}
 
   ngOnInit(): void {}
   logout() {
     this.loginService.clearSession();
+    // this.router.navigateByUrl('log-in'); // redirect to Log-in
   }
 }
