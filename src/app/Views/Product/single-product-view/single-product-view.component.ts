ba50d4a24fbaf99d4ceee43eea855b72d72fdfa8
naranjito72 <theclick36@gmail.com>
2021-04-12 13:28:40 +0200

    formcontrol

--- src/app/Views/Product/single-product-view/single-product-view.component.ts
+++ src/app/Views/Product/single-product-view/single-product-view.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ProductsService } from 'src/app/Services/products.service';
 import { faCartPlus, faClipboardCheck, faMinus, faPlus } from '@fortawesome/free-solid-svg-icons';
 import { faCheckCircle } from '@fortawesome/free-regular-svg-icons';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-single-product-view',
@@ -20,6 +21,17 @@ export class SingleProductViewComponent implements OnInit {
   faClipBoardCheck = faClipboardCheck;
   faCheckCircle = faCheckCircle;
 
+  // selectors
+  plusOne: HTMLElement = document.querySelector('#plusToBuy');
+  minusOne: HTMLElement = document.querySelector('#minusToBuy');
+  // input numeric
+  numberUnits: number = 0;
+
+  // reactive form
+  getUnitsInput = new FormGroup({
+    numberUnits: new FormControl('1')
+  });
+
   constructor(private productsService: ProductsService) { }
 
   ngOnInit(): void {
@@ -32,6 +44,12 @@ export class SingleProductViewComponent implements OnInit {
         error => {
           console.log(error);
         });
+
   }
+
+  getValueUnits(){
+
+  }
+
 
 }
