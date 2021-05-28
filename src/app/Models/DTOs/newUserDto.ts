b4847cb5e10a676db6d43a3904a6ae81aeebfd97
ConxiCr <conxicr@hotmail.com>
2021-05-28 14:26:01 +0200

    [F-77] Fix Sign Up Api Call Request. Class
    Signed-off-by: ConxiCr <conxicr@hotmail.com>

--- src/app/Models/DTOs/newUserDto.ts
+++ src/app/Models/DTOs/newUserDto.ts
@@ -1,21 +1,26 @@
 export class UserSignUpDto {
-
-  address: string;
-  dni: string;
-  image: string;
-  name_surname: string;
-  username: string;
-  password: string;
-  user_type: string;
+  dni             : string; 
+  image           : string;
+  name_and_surname: string;
+  password        : string;
+  username        : string;
+  address         : string;
+    street  : string;
+    number  : string;
+    city    : string;
+    country : string;
+    zipcode : string;
+  shipping_address?: string;
+  user_type        : string;
 
 
   constructor(newClient) {
-    this.address = `${newClient.address.inputAddress} ${newClient.address.inputCity} ${newClient.address.inputProvince} ${newClient.address.inputZIP} ${newClient.address.inputCountry}`;
     this.dni = newClient.inputDNI;
     this.image = '';
-    this.name_surname = `${newClient.inputName} ${newClient.inputSurname}`;
-    this.username = newClient.inputEmail;
+    this.name_and_surname = `${newClient.inputName} ${newClient.inputSurname}`;
     this.password = newClient.inputPassword;
+    this.username = newClient.inputEmail;
+    this.address = `${newClient.address.inputAddress} ${newClient.address.inputCity} ${newClient.address.inputProvince} ${newClient.address.inputZIP} ${newClient.address.inputCountry}`;
     this.user_type = 'CLIENT'
   }
 
