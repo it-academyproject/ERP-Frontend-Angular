5cb84cfcfcfa04529203e9e6d233abd60437161e
Ester79 <esterramoscuervo@hotmail.com>
2021-09-30 12:36:38 +0200

    Create offers.service.ts
    View Offers

---
+++ src/app/Services/offers.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { LoginService } from './login.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+
+export class OffersService {
+  url: string = 'http://217.76.158.200:8080';
+  endPoint: string = '/api/offers';
+  token: string;
+
+  offerById: Observable<any>;
+
+  constructor(private httpClient: HttpClient, private loginService: LoginService) {
+    this.token = this.loginService.getBearerToken;
+   }
+
+   headers: HttpHeaders = new HttpHeaders({
+     Authorization: this.loginService.getBearerToken
+   });
+
+   getAllOffers(){
+     return this.httpClient.get(`${this.url}${this.endPoint}`, {headers: this.headers});
+   }
+
+   getOfferById(id: string){
+     return (this.offerById = this.httpClient.get(`${this.url}${this.endPoint}/${id}`, {headers: this.headers}));
+   }
+}
+
